e72b737bde1b23c9b5408672dbc6e7a935705979
blessed1797 <blessedlyjummy@gmail.com>
2022-02-07 22:45:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+javascript:void(0);
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+
+javascript:void(0);
+var i = 0;
+function movee() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+javascript:void(0);
+var i = 0;
+function moveee() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar3");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+javascript:void(0);
+var i = 0;
+function moveeee() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar4");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
